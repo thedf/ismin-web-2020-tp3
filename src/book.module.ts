66a24d3543bef0a8f7963d429f015143d16afc5d
Fahd Lyousfi <fahdlyousfi@gmail.com>
2020-09-14 10:43:43 +0200

    Switching Database url from written to Env Param

--- src/book.module.ts
+++ src/book.module.ts
@@ -3,11 +3,10 @@ import { BookController } from './book.controller';
 import { BookService } from './book.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { BookDocument, BookSchema } from './Book.schema';
+require('dotenv').config();
 @Module({
   imports: [
-    MongooseModule.forRoot(
-      'mongodb+srv://fahd:fahd1234@cluster0.gzj2f.mongodb.net/tp3?retryWrites=true&w=majority',
-    ),
+    MongooseModule.forRoot(process.env.DATABASE_URL),
     MongooseModule.forFeature([
       { name: BookDocument.name, schema: BookSchema },
     ]),
